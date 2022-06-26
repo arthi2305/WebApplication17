8a573b822d896756fc97b601edf93ae85f02ab00
arthi2305 <HP@DESKTOP-PR29BJI>
2022-06-26 15:39:25 +0530

    Add project files.

---
+++ WebApplication17/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using WebAppliaction17.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication17.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+      
+        public ActionResult ContactForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication17/Models/MemebrModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppliaction17.Models
+{
+    public class MemberModel
+    {
+        public string Name { get; set; }
+        public string designation { get; set; }
+        public string ImagePath { get; set; }
+        public HttpPostedFile ImageFile { get; set; }
+    }
+}
